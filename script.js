c6d4c8cac126d8c18786a002b099b89958732497
Vigneshwaran S <85797380+v-i-k-y-01@users.noreply.github.com>
2023-07-16 11:50:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+<script>
+    function convert()
+    {
+        const cmVal = Number(document.getElementById("input").value)
+        const inchVal = cmVal/2.54
+        const result = document.getElementById("result")
+        result.innerHTML = inchVal.toFixed(3) + " inches"
+    }
+    
+</script>
